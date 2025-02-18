c0cabfe7569689ff982007996e4901ba6d372080
Christo Mitov <christo.mitov@gmail.com>
2025-02-18 17:41:00 -0500

    bug fixes

--- priv/repo/migrations/20250218214832_remove_join_leave_flags_from_sounds.exs
+++ priv/repo/migrations/20250218214832_remove_join_leave_flags_from_sounds.exs
@@ -2,6 +2,9 @@ defmodule Soundboard.Repo.Migrations.RemoveJoinLeaveFlagsFromSounds do
   use Ecto.Migration
 
   def change do
+    drop_if_exists index(:sounds, [:is_join_sound], name: :user_join_sound_index)
+    drop_if_exists index(:sounds, [:is_leave_sound], name: :user_leave_sound_index)
+
     alter table(:sounds) do
       remove :is_join_sound
       remove :is_leave_sound
