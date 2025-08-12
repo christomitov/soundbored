5aa623e68447eba04b550c5aa0c55ed2aa42b49f
Christo Mitov <christo.mitov@gmail.com>
2025-08-11 22:30:09 -0400

    Update Nostrum ref for audio playback fix

--- mix.exs
+++ mix.exs
@@ -93,7 +93,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:nostrum, github: "christomitov/nostrum", ref: "e778c10"},
+      {:nostrum, github: "christomitov/nostrum", ref: "ae3b95c"},
       {:ecto_sqlite3, "~> 0.18.0"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
