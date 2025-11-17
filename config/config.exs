70b4d3e78ea484af75a1e2d59f9895e9649b8f4c
Christo Mitov <christo.mitov@gmail.com>
2025-11-16 20:19:37 -0500

    support multiple wav mime type???

--- config/config.exs
+++ config/config.exs
@@ -73,6 +73,10 @@ config :mime, :types, %{
   "audio/x-m4a" => ["m4a"]
 }
 
+config :mime, :extensions, %{
+  "wav" => "audio/wav"
+}
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
