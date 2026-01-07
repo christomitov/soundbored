b99340fed529a05f254f0ee1cd23b95220c01ef0
root <root@CT116.local>
2026-01-07 20:37:41 +0000

    fix: use patched nostrum for voice decrypt (cleaned)

--- mix.exs
+++ mix.exs
@@ -93,7 +93,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2"},
       {:bandit, "~> 1.8"},
-      {:nostrum, github: "Kraigie/nostrum", branch: "master"},
+      {:nostrum, path: "deps_override/nostrum"},
       {:ecto_sqlite3, "~> 0.22"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
