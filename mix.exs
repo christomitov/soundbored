a2ce77809c98cbf3860f83005bbfa8afb1106708
Christo Mitov <christo.mitov@gmail.com>
2026-03-03 11:37:43 -0500

    Update EDA fork with binary DAVE MLS frame handling

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2"},
       {:bandit, "~> 1.8"},
-      {:eda, github: "christomitov/EDA", ref: "ae55156"},
+      {:eda, github: "christomitov/EDA", ref: "58d9bcf"},
       {:ecto_sqlite3, "~> 0.22"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
