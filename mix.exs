fe0644e4abf364c68b3124dc2fdbe092b3e7288c
Christo Mitov <christo.mitov@gmail.com>
2026-03-03 12:04:23 -0500

    Bump EDA ref for nested encrypt tuple fix

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2"},
       {:bandit, "~> 1.8"},
-      {:eda, github: "christomitov/EDA", ref: "f257ca5"},
+      {:eda, github: "christomitov/EDA", ref: "b1827f2"},
       {:ecto_sqlite3, "~> 0.22"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
