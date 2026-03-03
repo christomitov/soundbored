28b6ef22b24a7545eda6e08a1ae0f656d1d8625a
Christo Mitov <christo.mitov@gmail.com>
2026-03-03 13:16:46 -0500

    Bump EDA dependency to 89a9d2f

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2"},
       {:bandit, "~> 1.8"},
-      {:eda, github: "christomitov/EDA", ref: "2692944"},
+      {:eda, github: "christomitov/EDA", ref: "89a9d2f"},
       {:ecto_sqlite3, "~> 0.22"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
