fdbbcb5df69bec9c01a23e3f0d8b9d1bd5c666d9
Christo Mitov <christo.mitov@gmail.com>
2026-03-03 11:28:49 -0500

    Update EDA fork to propagate DAVE manager for playback

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2"},
       {:bandit, "~> 1.8"},
-      {:eda, github: "christomitov/EDA", ref: "2cd0630"},
+      {:eda, github: "christomitov/EDA", ref: "81903ba"},
       {:ecto_sqlite3, "~> 0.22"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
