e0320a6b2e3f9a57f1a8693d7c7a9011f404c658
Christo Mitov <christo.mitov@gmail.com>
2026-03-03 11:43:47 -0500

    Update EDA fork with voice session registry guard

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule Soundboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2"},
       {:bandit, "~> 1.8"},
-      {:eda, github: "christomitov/EDA", ref: "8d601a6"},
+      {:eda, github: "christomitov/EDA", ref: "bc1cffe"},
       {:ecto_sqlite3, "~> 0.22"},
       {:number, "~> 1.0"},
       {:ueberauth, "~> 0.10.5"},
